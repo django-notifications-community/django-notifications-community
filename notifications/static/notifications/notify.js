faa6951ae9c09356b3aa8d880a0b1d6e0e7afc28
je-bugshell <104762666+je-bugshell@users.noreply.github.com>
2026-04-12 21:38:32 +0200

    clean: modernize notify.js syntax
    Replace var with let/const for internal state, index-based for loops
    with for...of, string concatenation with template literals, and
    remove redundant if-guards around getElementsByClassName (which never
    returns null).
    
    Public API (global variables and function names) is unchanged.
    XHR, typeof checks, and auto-polling behavior are preserved.

--- notifications/static/notifications/notify.js
+++ notifications/static/notifications/notify.js
@@ -7,42 +7,37 @@ var notify_mark_all_unread_url;
 var notify_refresh_period = 15000;
 // Set notify_mark_as_read to true to mark notifications as read when fetched
 var notify_mark_as_read = false;
-var consecutive_misfires = 0;
-var registered_functions = [];
+
+let consecutive_misfires = 0;
+const registered_functions = [];
 
 function fill_notification_badge(data) {
-    var badges = document.getElementsByClassName(notify_badge_class);
-    if (badges) {
-        for (var i = 0; i < badges.length; i++) {
-            badges[i].innerHTML = data.unread_count;
-        }
+    for (const badge of document.getElementsByClassName(notify_badge_class)) {
+        badge.innerHTML = data.unread_count;
     }
 }
 
 function fill_notification_list(data) {
-    var menus = document.getElementsByClassName(notify_menu_class);
-    if (menus) {
-        var messages = data.unread_list.map(function (item) {
-            var message = "";
+    const messages = data.unread_list.map((item) => {
+        let message = '';
 
-            if (typeof item.actor !== 'undefined') {
-                message = item.actor;
-            }
-            if (typeof item.verb !== 'undefined') {
-                message = message + " " + item.verb;
-            }
-            if (typeof item.target !== 'undefined') {
-                message = message + " " + item.target;
-            }
-            if (typeof item.timestamp !== 'undefined') {
-                message = message + " " + item.timestamp;
-            }
-            return '<li>' + message + '</li>';
-        }).join('')
-
-        for (var i = 0; i < menus.length; i++) {
-            menus[i].innerHTML = messages;
+        if (typeof item.actor !== 'undefined') {
+            message = item.actor;
         }
+        if (typeof item.verb !== 'undefined') {
+            message += ` ${item.verb}`;
+        }
+        if (typeof item.target !== 'undefined') {
+            message += ` ${item.target}`;
+        }
+        if (typeof item.timestamp !== 'undefined') {
+            message += ` ${item.timestamp}`;
+        }
+        return `<li>${message}</li>`;
+    }).join('');
+
+    for (const menu of document.getElementsByClassName(notify_menu_class)) {
+        menu.innerHTML = messages;
     }
 }
 
@@ -51,10 +46,9 @@ function register_notifier(func) {
 }
 
 function fetch_api_data() {
-    // only fetch data if a function is setup
     if (registered_functions.length > 0) {
-        var r = new XMLHttpRequest();
-        var params = '?max=' + notify_fetch_count;
+        const r = new XMLHttpRequest();
+        let params = `?max=${notify_fetch_count}`;
 
         if (notify_mark_as_read) {
             params += '&mark_as_read=true';
@@ -64,9 +58,9 @@ function fetch_api_data() {
             if (this.readyState === 4) {
                 if (this.status === 200) {
                     consecutive_misfires = 0;
-                    var data = JSON.parse(r.responseText);
-                    for (var i = 0; i < registered_functions.length; i++) {
-                       registered_functions[i](data);
+                    const data = JSON.parse(r.responseText);
+                    for (const func of registered_functions) {
+                        func(data);
                     }
                 } else {
                     consecutive_misfires++;
@@ -79,12 +73,9 @@ function fetch_api_data() {
     if (consecutive_misfires < 10) {
         setTimeout(fetch_api_data, notify_refresh_period);
     } else {
-        var badges = document.getElementsByClassName(notify_badge_class);
-        if (badges) {
-            for (var i = 0; i < badges.length; i++) {
-                badges[i].innerHTML = "!";
-                badges[i].title = "Connection lost!"
-            }
+        for (const badge of document.getElementsByClassName(notify_badge_class)) {
+            badge.innerHTML = "!";
+            badge.title = "Connection lost!";
         }
     }
 }
