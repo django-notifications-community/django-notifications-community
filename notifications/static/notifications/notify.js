719412975d95b39ed6dcbdb6981b987d7b7e174c
je-bugshell <104762666+je-bugshell@users.noreply.github.com>
2026-04-12 18:08:28 +0200

    clean: modernize notify.js syntax
    Replace XMLHttpRequest with fetch/async-await, var with let/const
    for internal state, for loops with for...of, string concatenation
    with template literals, and add try/catch for network errors.
    
    Public API (global variables and function names) is unchanged.

--- notifications/static/notifications/notify.js
+++ notifications/static/notifications/notify.js
@@ -7,42 +7,37 @@ var notify_mark_all_unread_url;
 var notify_refresh_period = 15000;
 // Set notify_mark_as_read to true to mark notifications as read when fetched
 var notify_mark_as_read = false;
-var consecutive_misfires = 0;
-var registered_functions = [];
+
+let consecutive_misfires = 0;
+const registered_functions = [];
 
 function fill_notification_badge(data) {
-    var badges = document.getElementsByClassName(notify_badge_class);
-    if (badges) {
-        for (var i = 0; i < badges.length; i++) {
-            badges[i].innerHTML = data.unread_count;
-        }
+    for (const badge of document.getElementsByClassName(notify_badge_class)) {
+        badge.innerHTML = data.unread_count;
     }
 }
 
 function fill_notification_list(data) {
-    var menus = document.getElementsByClassName(notify_menu_class);
-    if (menus) {
-        var messages = data.unread_list.map(function (item) {
-            var message = "";
+    const messages = data.unread_list.map((item) => {
+        let message = '';
 
-            if (typeof item.actor !== 'undefined') {
-                message = item.actor;
-            }
-            if (typeof item.verb !== 'undefined') {
-                message = message + " " + item.verb;
-            }
-            if (typeof item.target !== 'undefined') {
-                message = message + " " + item.target;
-            }
-            if (typeof item.timestamp !== 'undefined') {
-                message = message + " " + item.timestamp;
-            }
-            return '<li>' + message + '</li>';
-        }).join('')
-
-        for (var i = 0; i < menus.length; i++) {
-            menus[i].innerHTML = messages;
+        if (item.actor) {
+            message = item.actor;
         }
+        if (item.verb) {
+            message += ` ${item.verb}`;
+        }
+        if (item.target) {
+            message += ` ${item.target}`;
+        }
+        if (item.timestamp) {
+            message += ` ${item.timestamp}`;
+        }
+        return `<li>${message}</li>`;
+    }).join('');
+
+    for (const menu of document.getElementsByClassName(notify_menu_class)) {
+        menu.innerHTML = messages;
     }
 }
 
@@ -50,41 +45,35 @@ function register_notifier(func) {
     registered_functions.push(func);
 }
 
-function fetch_api_data() {
-    // only fetch data if a function is setup
+async function fetch_api_data() {
     if (registered_functions.length > 0) {
-        var r = new XMLHttpRequest();
-        var params = '?max=' + notify_fetch_count;
-
+        let params = `?max=${notify_fetch_count}`;
         if (notify_mark_as_read) {
             params += '&mark_as_read=true';
         }
 
-        r.addEventListener('readystatechange', function(event) {
-            if (this.readyState === 4) {
-                if (this.status === 200) {
-                    consecutive_misfires = 0;
-                    var data = JSON.parse(r.responseText);
-                    for (var i = 0; i < registered_functions.length; i++) {
-                       registered_functions[i](data);
-                    }
-                } else {
-                    consecutive_misfires++;
+        try {
+            const response = await fetch(notify_api_url + params);
+            if (response.ok) {
+                consecutive_misfires = 0;
+                const data = await response.json();
+                for (const func of registered_functions) {
+                    func(data);
                 }
+            } else {
+                consecutive_misfires++;
             }
-        });
-        r.open("GET", notify_api_url + params, true);
-        r.send();
+        } catch {
+            consecutive_misfires++;
+        }
     }
+
     if (consecutive_misfires < 10) {
         setTimeout(fetch_api_data, notify_refresh_period);
     } else {
-        var badges = document.getElementsByClassName(notify_badge_class);
-        if (badges) {
-            for (var i = 0; i < badges.length; i++) {
-                badges[i].innerHTML = "!";
-                badges[i].title = "Connection lost!"
-            }
+        for (const badge of document.getElementsByClassName(notify_badge_class)) {
+            badge.innerHTML = '!';
+            badge.title = 'Connection lost!';
         }
     }
 }
